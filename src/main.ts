dcb23efb12a3d46cd3c6027220f0e03656a71905
Kevin Daniel Morales Estrella <kdmorale@pichincha.com>
2024-09-20 18:43:10 -0600

    agregar responsabilidad

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,18 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import * as hbs from 'hbs';
+//const cors = require("cors");
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
   hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
+  const corsOptions = {
+    origin: '*', // Replace with the allowed domain
+  };
+  app.use(cors(corsOptions));
 
   await app.listen(3000);
 }
